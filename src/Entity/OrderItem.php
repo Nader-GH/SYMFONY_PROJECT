c870cf1aed7b0748f0198b714f0b0dd7ef06aecd
Mohamed Mehdi Frikha <144732642+exotic-zenith@users.noreply.github.com>
2025-05-30 13:04:10 +0100

    create order and OrderItem entities
    added entities to store finalized orders, including user, items, total price, status and date

---
+++ src/Entity/OrderItem.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+class OrderItem
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\ManyToOne(targetEntity: Order::class, inversedBy: 'items')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $order;
+
+    #[ORM\ManyToOne(targetEntity: Product::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private $product;
+
+    #[ORM\Column(type: 'integer')]
+    private $quantity;
+
+    #[ORM\Column(type: 'float')]
+    private $price;
+
+  
+}
