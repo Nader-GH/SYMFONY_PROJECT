ff444c11e2191c548896cd469164b09e765ad068
Mohamed Mehdi Frikha <144732642+exotic-zenith@users.noreply.github.com>
2025-05-30 23:20:36 +0100

    added cart entity repository

---
+++ src/Entity/Cart.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\User;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+class Cart
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private $user;
+
+    #[ORM\Column(type: 'json')]
+    private $items = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): self
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    public function setItems(array $items): self
+    {
+        $this->items = $items;
+        return $this;
+    }
+}
