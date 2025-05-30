961f8c6befa49e6cf9d48c68e2331d55a10bab36
Louay <louaysaidani1@gmail.com>
2025-05-31 00:10:02 +0100

    Add product with search and category filter

---
+++ migrations/Version20250530220537.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250530220537 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE yes (id INT AUTO_INCREMENT NOT NULL, yes VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT '(DC2Type:json)', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_YES (yes), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE product ADD image_name VARCHAR(255) DEFAULT NULL, DROP image, CHANGE updated_at updated_at DATETIME NOT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            DROP TABLE yes
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE product ADD image VARCHAR(255) NOT NULL, DROP image_name, CHANGE updated_at updated_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)'
+        SQL);
+    }
+}
