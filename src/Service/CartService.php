49f404f615369f5b8e11a2fb3ae9cfbd94ffa34f
Mohamed Mehdi Frikha <144732642+exotic-zenith@users.noreply.github.com>
2025-05-30 23:21:01 +0100

    update CartService for persistant cart

--- src/Service/CartService.php
+++ src/Service/CartService.php
@@ -2,55 +2,73 @@
 
 namespace App\Service;
 
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Entity\Cart;
+use App\Entity\User;
+use App\Repository\CartRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class CartService
 {
-    private $session;
+    private $requestStack;
+    private $entityManager;
+    private $cartRepository;
     private const CART_KEY = 'cart';
 
-    public function __construct(SessionInterface $session)
+    public function __construct(RequestStack $requestStack, EntityManagerInterface $entityManager, CartRepository $cartRepository)
     {
-        $this->session = $session;
+        $this->requestStack = $requestStack;
+        $this->entityManager = $entityManager;
+        $this->cartRepository = $cartRepository;
+    }
+
+    private function getSession()
+    {
+        return $this->requestStack->getSession();
     }
 
     public function add(int $productId, int $quantity = 1): void
     {
-        $cart = $this->session->get(self::CART_KEY, []);
+        $session = $this->getSession();
+        $cart = $session->get(self::CART_KEY, []);
         if (isset($cart[$productId])) {
             $cart[$productId] += $quantity;
         } else {
             $cart[$productId] = $quantity;
         }
-        $this->session->set(self::CART_KEY, $cart);
+        $session->set(self::CART_KEY, $cart);
     }
 
     public function remove(int $productId): void
     {
-        $cart = $this->session->get(self::CART_KEY, []);
+        $session = $this->getSession();
+        $cart = $session->get(self::CART_KEY, []);
         unset($cart[$productId]);
-        $this->session->set(self::CART_KEY, $cart);
+        $session->set(self::CART_KEY, $cart);
     }
 
     public function update(int $productId, int $quantity): void
     {
-        $cart = $this->session->get(self::CART_KEY, []);
+        $session = $this->getSession();
+        $cart = $session->get(self::CART_KEY, []);
         if ($quantity > 0) {
             $cart[$productId] = $quantity;
         } else {
             unset($cart[$productId]);
         }
-        $this->session->set(self::CART_KEY, $cart);
+        $session->set(self::CART_KEY, $cart);
     }
 
     public function getCart(): array
     {
-        return $this->session->get(self::CART_KEY, []);
+        $session = $this->getSession();
+        return $session->get(self::CART_KEY, []);
     }
 
     public function clear(): void
     {
-        $this->session->remove(self::CART_KEY);
+        $session = $this->getSession();
+        $session->remove(self::CART_KEY);
     }
 
     public function getTotalPrice(array $products): float
@@ -64,5 +82,27 @@ class CartService
             }
         }
         return $total;
+    }
+
+    public function saveCartForUser(User $user): void
+    {
+        $sessionCart = $this->getCart();
+        $cart = $this->cartRepository->findOneBy(['user' => $user]);
+        if (!$cart) {
+            $cart = new Cart();
+            $cart->setUser($user);
+        }
+        $cart->setItems($sessionCart);
+        $this->entityManager->persist($cart);
+        $this->entityManager->flush();
+    }
+
+    public function loadCartForUser(User $user): void
+    {
+        $cart = $this->cartRepository->findOneBy(['user' => $user]);
+        if ($cart) {
+            $session = $this->getSession();
+            $session->set(self::CART_KEY, $cart->getItems());
+        }
     }
 }
